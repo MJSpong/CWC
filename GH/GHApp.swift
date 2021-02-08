2ad035bfe0d897f7dd6c3441d6347587727b6dbd
Matt <matt.spong@gmail.com>
2021-02-08 13:44:50 +1100

    Initial Commit

---
+++ GH/GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHApp.swift
+//  GH
+//
+//  Created by Matthew Spong on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
